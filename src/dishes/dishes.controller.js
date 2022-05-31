085e03953727fa9311ea8dc4403f5fd26f51f495
Shawn <smoreo22@aol.com>
2022-05-31 14:55:21 -0400

    added more routing functionality

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,3 +1,4 @@
+const { builtinModules } = require("module");
 const path = require("path");
 
 // Use the existing dishes data
@@ -7,3 +8,92 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+function create(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Dish must include a ${propertyName}` });
+  };
+}
+function priceIsValid(req, res, next) {
+  const { data: { price } = {} } = req.body;
+  if (price <= 0 || !Number.isInteger(price)) {
+    return next({
+      status: 400,
+      message: `Dish must have a price that is an integer greater than 0`,
+    });
+  }
+  next();
+}
+
+function dishExists(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Dish id not found ${dishId}`,
+  });
+}
+
+function read(req, res) {
+  res.json({ data: res.locals.dish });
+}
+
+function update(req, res) {
+  const dish = res.locals.dish;
+  const { data: { id, name, description, price, image_url } = {} } = req.body;
+
+  foundDish.id = id;
+  foundDish.name = name;
+  foundDish.description = description;
+  foundDish.price = price;
+  foundDish.image_url = image_url;
+
+  res.json({ data: paste });
+}
+
+module.exports = {
+  list,
+  create: [
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    priceIsValid,
+    create,
+  ],
+  read: [dishExists, read],
+  update: [
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    priceIsValid,
+    dishExists,
+    update
+  ],
+};
